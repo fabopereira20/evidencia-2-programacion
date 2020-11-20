3cc33a58731aa7c13198f4f4479f31785faa7f3c
fabopereira20 <73892102+fabopereira20@users.noreply.github.com>
2020-11-19 21:14:33 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.print("Hola mundo!");
+        System.out.println("Hola mundo!");
+        System.out.println("Fabián Pereira Hernández");
     }
 }
